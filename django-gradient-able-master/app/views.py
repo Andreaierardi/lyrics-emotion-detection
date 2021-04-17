8e7316d89aa94395f5e8fc70aef6bbf007f9b97c
Andrea Ierardi <andreierardi@gmail.com>
2021-04-17 17:53:43 +0200

    edit the template

--- django-gradient-able-master/app/views.py
+++ django-gradient-able-master/app/views.py
@@ -9,7 +9,7 @@ from django.template import loader
 from django.http import HttpResponse
 from django import template
 
-@login_required(login_url="/login/")
+#@login_required(login_url="/login/")
 def index(request):
     
     context = {}
@@ -18,7 +18,7 @@ def index(request):
     html_template = loader.get_template( 'index.html' )
     return HttpResponse(html_template.render(context, request))
 
-@login_required(login_url="/login/")
+#@login_required(login_url="/login/")
 def pages(request):
     context = {}
     # All resource paths end in .html.
