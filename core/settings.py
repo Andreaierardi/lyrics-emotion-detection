d313e2ce9931759332727275cad5a76a5c502028
Andrea Ierardi <andreierardi@gmail.com>
2021-04-19 15:42:14 +0200

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 # load production server from .env
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
+ALLOWED_HOSTS = ['localhost', 'lyricsemotiondetection.herokuapp.com','127.0.0.1', config('SERVER', default='127.0.0.1')]
 
 # Application definition
 
