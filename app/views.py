8786e763d76e12096de0bea29d0bfb53f8e51e9d
andreaierardi <andreierardi@gmail.com>
2021-04-25 12:26:31 +0200

    remove utils

--- app/views.py
+++ app/views.py
@@ -23,16 +23,6 @@ import pickle
 from sklearn import preprocessing
 import joblib
 
-
-from sklearn.base import BaseEstimator
-from sklearn.base import TransformerMixin
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from nltk.stem import SnowballStemmer
-
-import pandas as pd
-from utils import custom_transformer
-
 import nltk
 from sklearn.base import BaseEstimator
 from sklearn.base import TransformerMixin
@@ -41,17 +31,24 @@ from nltk.tokenize import word_tokenize
 from nltk.stem import SnowballStemmer
 nltk.download('stopwords')
 
+import tensorflow as tf
+with tf.compat.v1.Session():
+        from keras.models import load_model
+        
+from sklearn.pipeline import Pipeline
+from sklearn.pipeline import FeatureUnion
+nltk.download('punkt')
+# FeatureUnion combines two or more pipelines or transformers
+# and is very fast!
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_selection import SelectKBest, chi2
+from sklearn.preprocessing import StandardScaler
+
+
 # Custom Transformer (Inheriting from classes)
 class CleanText( BaseEstimator, TransformerMixin ):
     
-    # Class Constructor
-    # The class constructor is formed by a function with double underscore __ :
-    # these are called 'special functions' as they have special meaning.
-    # In particular the '__init__' gets called whenever
-    # a new object of that class is instantiated,
-    # and are used to initialize all the necessary variables.
-    # In this example we initialize the language variable 'lang' with 'English'
-    # and pick the SnowballStemmer as the default stemmer.
+  
     def __init__( self, lang = "english"):
         self.lang = lang
         self.stemmer = SnowballStemmer(self.lang)
@@ -74,8 +71,7 @@ class CleanText( BaseEstimator, TransformerMixin ):
     def transform( self, X, y = None ):
         return X["text"].apply(self.clean)
 
-# Custom Transformer: same parts as the previous custom transformer
-# This one will be used for feature extraction
+
 
 class CustomFeatures( BaseEstimator, TransformerMixin ):
     
@@ -97,24 +93,7 @@ class CustomFeatures( BaseEstimator, TransformerMixin ):
         f['punct']  = X['text'].str.findall(r'[^a-zA-Z\d\s:]').str.len()
         return f[['len','digits','punct']]
 
-from sklearn.pipeline import Pipeline
-from sklearn.pipeline import FeatureUnion
-nltk.download('punkt')
-# FeatureUnion combines two or more pipelines or transformers
-# and is very fast!
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_selection import SelectKBest, chi2
-from sklearn.preprocessing import StandardScaler
-# Our first pipeline called 'pipe' will be formed by three 'steps' or parts:
-# 1)"extract" which in turns is formed through FeatureUnion which put together two parts:
-# "terms" (formed by a pipeline with the CleanText() transformer we created above
-# and the TfidVectorize text vectorizing transformer from scikit-learn) and "custom"
-# (formed by the CustomFeatures transformer we created above);
-# 2) "select", formed by the scikit-learn transformer method "SelectKBest" for feature
-# selection with a chi squared score function;
-# 3) "scale", same as 2) using the StandardScaler method from scikit-learn.
-# The whole pipeline will be used as pre-processing task in classifying pipelines.
-# extract features
+
 pipe_bi = Pipeline([("extract", FeatureUnion([("terms", Pipeline([('clean', CleanText()),
                                                                ('tfidf', TfidfVectorizer(ngram_range = (1,2)))])),
                                            ("custom", CustomFeatures())])),
@@ -122,12 +101,10 @@ pipe_bi = Pipeline([("extract", FeatureUnion([("terms", Pipeline([('clean', Clea
                  ("scale", StandardScaler(with_mean = False))])
 
 
-#import tensorflow
 
 cv_RF = pickle.load(open('model_pickles/RandomForestCV.sav', 'rb'))
 cv_MLP= pickle.load(open('model_pickles/MLP.sav', 'rb'))
 
-import tensorflow as tf
 
 cv_logistic = pickle.load(open('model_pickles/logisticCV.sav', 'rb'))
 encoder = preprocessing.LabelEncoder()
@@ -141,9 +118,6 @@ pipe_bi.fit(x_train.drop("weigth",axis=1),y_train)
 
 print(cv_logistic)
 
-with tf.compat.v1.Session():
-        from keras.models import load_model
-        
 
 def get_emotion(request, lyrics):
 
